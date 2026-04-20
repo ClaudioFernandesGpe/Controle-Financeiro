5725adf2876b0f34b2ed7f59541956054905175e
ClaudioFernandesGpe <claudiofernadesgpe@gmail.com>
2026-04-20 15:45:50 -0300

    feat: initial structure and fake db, model and controller add

---
+++ app/controller/financeiro_controller.py
@@ -0,0 +1,19 @@
+from app.database.fake_db import transacoes
+from app.model.transacao_model import Transacao
+
+def listar_transacoes():
+    return transacoes
+
+def criar_transacao(transacao: Transacao):
+    transacoes.append(transacao.model_dump())
+    return transacao
+
+def calcular_saldo():
+    saldo = 0
+    for transacao in transacoes:
+        if transacao["tipo"] == "entrada":
+            saldo += transacao["valor"]
+        else:
+            saldo -= transacao["valor"]
+        return {"saldo": saldo}
+        
---
+++ app/database/fake_db.py
@@ -0,0 +1,23 @@
+transacoes = [
+    {
+        "id": 1,
+        "descricao": "Salário",
+        "valor": 2900,
+        "tipo": "entrada",
+        "categoria": "Renda"
+    },
+    {
+        "id": 2,
+        "descricao": "Mercado",
+        "valor": 680,
+        "tipo": "saida",
+        "categoria": "Alimentação"
+    },
+    {
+        "id": 3,
+        "descricao": "Conta de Luz",
+        "valor": 116,
+        "tipo": "saida",
+        "categoria": "Moradia"
+    },
+]
